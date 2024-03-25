4f990739bfd588e3b4e8e18ec81d8bbd408b87b0
rtk29 <rtk29bath@gmail.com>
2024-03-25 12:18:52 +0000

    Server, client files update

--- main/client.py
+++ main/client.py
@@ -33,8 +33,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Fetch the API key from environment variables
-#api_key = os.environ['OPENAI_API_KEY']
-api_key = os.getenv('OPENAI_API_KEY')
+api_key = os.environ['OPENAI_API_KEY']
+#api_key = os.getenv('OPENAI_API_KEY')
 
 # Initializes a Flask web application
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
